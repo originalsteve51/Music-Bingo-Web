73673ee84575c914cfbfc0eab56388a4dc7886ab
Stephen Harding <ssh751@gmail.com>
2024-12-26 13:59:12 -0500

    make scripts handle conda env

--- web_control/dev_script.sh
+++ web_control/dev_script.sh
@@ -4,6 +4,8 @@
 
 # You need to activate flask311 virtual environment first
 # conda activate flask311
+# eval "$(conda shell.bash hook)"
+# conda activate flask311
 
 # RUN_ON_HOST:USING_PORT forms the URL where JavaScript posts are directed
 # export RUN_ON_HOST="svpserver5.ddns.net"
